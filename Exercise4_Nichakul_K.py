3640122225b0bee52af83b1431da9c42569ceb75
MinuteW <mintechno54@gmail.com>
2022-01-03 19:50:50 +0700

    Create Exercise4_Nichakul_K.py

---
+++ Exercise4_Nichakul_K.py
@@ -0,0 +1,11 @@
+FE = 55.5
+GB = 60.0
+ICS = 77.5
+CP = 85.0
+
+print("--- Your Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
+print("------------------")
